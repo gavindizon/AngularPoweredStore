a6b076ef656161bc917b1be07362a3181c2bda78
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2020-06-01 22:56:55 +0800

    added component and model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div class="bg-success p-2 text-center text-white">
-                This is the Store
-            </div>`
+  template: `<store></store>`
   //templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css']
 })
