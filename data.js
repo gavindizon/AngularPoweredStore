97dcc1f7c2363b9cbd93c466ca87cd089ab918da
GavinRaineDizon <60138634+GavinRaineDizon@users.noreply.github.com>
2020-06-01 12:27:56 +0800

    added dependencies and initialized project

---
+++ data.js
@@ -0,0 +1,72 @@
+module.exports = () => {
+    return{
+        products:[
+            {
+                id: 1,
+               name: "Chocolate Frogs",
+                category: "Sweets",
+                description: "Miranda Goshawk",
+                price: 275
+            },
+            {
+                id: 2,
+                name: "Book of Spells",
+                category: "Eco-critical Realness",
+                description: "Miranda Goshawk",
+                price: 499.99
+            },
+            {   id: 3,
+                name: "Cloak of Invisibility",
+                category: "Accessories",
+                description: "Harry's Cloak",
+                price: 5000
+            },
+            {
+                id: 4,
+                name: "Elder Wand",
+                category: "Wand",
+                description: "Ollivander's",
+                price: 2999.99
+            },
+            {
+                id: 5,
+                name: "Dragon Hide Gloves",
+                category: "Uniform",
+                description: "Protective Gloves",
+                price: 2999.99
+            },
+            {
+                id: 6,
+                name: "Plain Work robes",
+                category: "Uniform",
+                description: "Black",
+                price: 800.5
+            },
+            {
+                id: 7,
+                name: "Voyages with Vampires",
+                category: "Book",
+                description: "Gilderoy Lockhart",
+                price: 450.2
+            },
+            {
+                id: 8,
+                name: "A History of Magic",
+                category: "Book",
+                description: "Bathilda Bagshot",
+                price: 240.33
+            },
+            {
+                id: 9,
+                name: "Fantastic Beasts and Where to Find Them",
+                category: "Book",
+                description: "Newt Scamander",
+                price: 324.5
+            },
+            
+        ],
+        orders: [
+            
+        ]
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div class="bg-success p-2 text-center text-white">
+                This is the Store
+            </div>`
+  //templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angularpoweredstore';
