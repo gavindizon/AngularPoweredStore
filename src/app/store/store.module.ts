851bc7e7ec94aaf60773a82cd3c45beb34e25efe
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2020-06-02 20:57:04 +0800

    added routing

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -5,11 +5,14 @@ import { ModelModule } from "../model/model.module";
 import { StoreComponent } from "./store.component";
 import { CounterDirective } from "./counter.directive";
 import { cartSummaryComponent } from "./cartSummary.component";
+import { CartDetailComponent } from "./cartDetail.component";
+import { CheckoutComponent } from "./checkout.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-    imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent, CounterDirective, cartSummaryComponent],
-    exports: [StoreComponent]
+    imports: [ModelModule, BrowserModule, FormsModule, RouterModule],
+    declarations: [StoreComponent, CounterDirective, cartSummaryComponent, CartDetailComponent, CheckoutComponent],
+    exports: [StoreComponent, CartDetailComponent, CheckoutComponent]
 })
 
 export class StoreModule {}
